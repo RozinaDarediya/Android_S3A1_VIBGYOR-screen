24f1740371e6f81df4d72367013b1d7c33c52858
RozinaDarediya <rozy_darediya@ymail.com>
2017-04-08 20:01:31 +0530

    Initial commit

---
+++ app/src/main/java/com/example/dell/session2assignment1/LinierLayoutJavaClass.java
@@ -0,0 +1,16 @@
+package com.example.dell.session2assignment1;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by dell on 10/23/2016.
+ */
+public class LinierLayoutJavaClass  extends AppCompatActivity{
+@Override
+protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.linear_layout_example);
+        }
+        }
