0b91ef3c04aad9094702d2a2b7f981fe42bd534d
RozinaDarediya <rozy_darediya@ymail.com>
2017-04-27 14:27:02 +0530

    Added the comment

--- app/src/main/java/com/example/dell/session2assignment1/MainActivity.java
+++ app/src/main/java/com/example/dell/session2assignment1/MainActivity.java
@@ -9,5 +9,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /*
+        This application shows the VIBGYOR screen using linear layout.
+        It will appear as the application is opened.
+         */
     }
 }
